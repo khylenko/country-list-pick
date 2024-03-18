d286e7d8a1d3523f008d952b9ad7a0733f689dac
khylenko <khylenko.i.v@gmail.com>
2024-03-18 11:36:43 +0200

    Update selection_list.dart
    added pickerBackgroundColor to selectionList

--- lib/selection_list.dart
+++ lib/selection_list.dart
@@ -75,7 +75,7 @@ class _SelectionListState extends State<SelectionList> {
     Widget scaffold = Scaffold(
       appBar: widget.appBar,
       body: Container(
-        color: Color(0xfff4f4f4),
+        color: widget.theme.pickerBackgroundColor ?? Color(0xfff4f4f4),
         child: LayoutBuilder(builder: (context, contrainsts) {
           diff = height - contrainsts.biggest.height;
           _heightscroller = (contrainsts.biggest.height) / _alphabet.length;
